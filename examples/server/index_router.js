faf27d422b62d5178c26a88bf81c510d020c81c0
Michael Paulson <mpaulson@netflix.com>
2015-06-12 10:50:19 -0600

    the router update.

--- examples/server/index_router.js
+++ examples/server/index_router.js
@@ -1,6 +1,6 @@
 var NetflixRouter = require('./netflixRouter');
 
-app.use('/member.json', middleware(function(req, res) {
+app.use('/member.json', dataSourceRoute(function(req, res) {
     return new Model({
         //cache: cache, No more cache!
         source: new NetflixRouter(req, res)
